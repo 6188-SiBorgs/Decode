8bff2c99471ce23288da68a419c1d5a7afc89c83
NytePhoenix <np2020.ax@gmail.com>
2025-10-05 10:00:25 -0700

    10.5.2025 turn correction for teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -16,13 +16,23 @@ public class Teleop extends LinearOpMode {
 
     public void runOpMode(){
         Chassis chassis = new Chassis(hardwareMap);
+        double targetAngle = chassis.yawDeg;
         waitForStart();
         while (opModeIsActive()){
             double px = gamepad1.left_stick_x;
             double py = -gamepad1.left_stick_y;
             double pa = gamepad1.right_stick_x;
+            double rot = pa;
             chassis.update(telemetry);
-            chassis.moveFieldRelative(px, py, pa);
+            if(pa != 0) {
+                targetAngle = chassis.yawDeg;
+            } else {
+                double angle = (targetAngle - chassis.yawDeg + 180) % 360 - 180;
+                if(Math.abs(angle) > 1) {
+                    rot = Math.max(-1.0, Math.min(-angle / 45.0, 1.0));
+                }
+            }
+            chassis.moveFieldRelative(px, py, rot);
             telemetry.update();
         }
     }
