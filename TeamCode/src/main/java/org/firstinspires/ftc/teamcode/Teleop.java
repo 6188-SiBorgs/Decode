34a46d25883dc279f16303f9f840ae394d25dfa3
NytePhoenix <np2020.ax@gmail.com>
2025-10-12 10:52:41 -0700

    10.12.2025 launcher code for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -16,6 +16,13 @@ public class Teleop extends LinearOpMode {
 
     public void runOpMode(){
         Chassis chassis = new Chassis(hardwareMap);
+        DcMotorEx right = hardwareMap.get(DcMotorEx.class, "right");
+        DcMotorEx left = hardwareMap.get(DcMotorEx.class, "left");
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
         while (opModeIsActive()){
             double px = gamepad1.left_stick_x;
@@ -23,6 +30,13 @@ public class Teleop extends LinearOpMode {
             double pa = gamepad1.right_stick_x;
             chassis.update(telemetry);
             chassis.moveFieldRelative(px, py, pa);
+            if(gamepad1.right_bumper) {
+                right.setVelocity(2000);
+                left.setVelocity(2000);
+            } else {
+                right.setVelocity(0);
+                left.setVelocity(0);
+            }
             telemetry.update();
         }
     }
